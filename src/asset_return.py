fa115786255b5af416a3398ee40c4ba017850a84
Felippe Raposo <raposo.felippe@gmail.com>
2018-01-08 13:34:51 -0200

    Round number

--- src/asset_return.py
+++ src/asset_return.py
@@ -1,3 +1,3 @@
 
 def calculate_future_asset_return(future_stock_price, current_stock_price):
-    return (future_stock_price - current_stock_price) / current_stock_price
+    return round((future_stock_price - current_stock_price) / current_stock_price, 1)
